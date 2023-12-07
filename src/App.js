efe0f5b7b675eac6ac0a32dbc19d3b116c03f108
Carl Bergenhem <cbergenhem@gmail.com>
2023-12-07 09:41:00 -0500

    Test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>Test McTest</div>
+        <div>Test McTest, but now more</div>
         <div>What magic does this hold?</div>
       </header>
     </div>
